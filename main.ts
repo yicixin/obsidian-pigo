f28bfe581b25b2efaef982b492147c5428fd99be
壹次心 <1306049032@qq.com>
2024-04-20 23:10:36 +0800

    upload to alioss

--- main.ts
+++ main.ts
@@ -1,81 +1,60 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Editor, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import OSS from 'ali-oss';
+import * as path from 'path';
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	accessKeyID: string;
+	accessKeySecret: string;
+	endpoint: string;
+	bucket: string;
+	dir: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	accessKeyID: "",
+	accessKeySecret: "",
+	endpoint: "",
+	bucket: "",
+	dir: ""
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	client: OSS;
 
 	async onload() {
 		await this.loadSettings();
+		this.initOssClient();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello world!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerEvent(
+			this.app.workspace.on('editor-paste', async (evt: ClipboardEvent, editor: Editor) => {
+				// 阻止默认的粘贴处理
+				evt.preventDefault();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				// 检查粘贴板数据是否包含图片
+				if (evt.clipboardData && evt.clipboardData.files.length > 0) {
+					const files = Array.from(evt.clipboardData.files).filter(file => file.type.startsWith('image/'));
+
+					if (files.length > 0) {
+						// 处理所有图片文件
+						for (const file of files) {
+							const imageUrl = await this.uploadImage(file);
+							console.log("upload success: " + imageUrl);
+							this.insertImageUrl(editor, file.name, imageUrl);
+						}
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+				if (evt.clipboardData){
+				// 如果没有图片，则按常规文本处理粘贴
+				const text = evt.clipboardData.getData('text');
+				editor.replaceSelection(text);
+				}
+			})
+		);
 	}
 
 	onunload() {
@@ -89,25 +68,35 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	initOssClient() {
+		this.client = new OSS({
+			accessKeyId: this.settings.accessKeyID,
+			accessKeySecret: this.settings.accessKeySecret,
+			endpoint: this.settings.endpoint,
+			bucket: this.settings.bucket
+		});
+	}
+
+	insertImageUrl(editor: Editor, filename: string, url: string) {
+		// Create the markdown string for the image
+		const imageMarkdown = `![${filename}](${url})`;
+		// Insert the image markdown at the current cursor position
+		editor.replaceSelection(imageMarkdown);
+	}
+
+	async uploadImage(file: File): Promise<string> {
+		const extension = path.extname(file.name);
+		const date = new Date().toISOString().split('T')[0];
+		const randomString = Math.random().toString(36).substring(2, 15);
+		const name = `${date}-${randomString}${extension}`;
+
+		const result = await this.client.put(this.settings.dir + name, file);
+		return result.url;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -115,20 +104,67 @@ class SampleSettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	display() {
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'My Plugin Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Access Key ID')
+			.setDesc('The Access Key ID for API access')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your Access Key ID')
+				.setValue(this.plugin.settings.accessKeyID)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.accessKeyID = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Access Key Secret')
+			.setDesc('The Access Key Secret for API access')
+			.addText(text => text
+				.setPlaceholder('Enter your Access Key Secret')
+				.setValue(this.plugin.settings.accessKeySecret)
+				.onChange(async (value) => {
+					this.plugin.settings.accessKeySecret = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Endpoint')
+			.setDesc('The API endpoint URL')
+			.addText(text => text
+				.setPlaceholder('Enter the API endpoint URL')
+				.setValue(this.plugin.settings.endpoint)
+				.onChange(async (value) => {
+					this.plugin.settings.endpoint = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Bucket')
+			.setDesc('The storage bucket name')
+			.addText(text => text
+				.setPlaceholder('Enter the bucket name')
+				.setValue(this.plugin.settings.bucket)
+				.onChange(async (value) => {
+					this.plugin.settings.bucket = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Directory')
+			.setDesc('The default directory for uploads')
+			.addText(text => text
+				.setPlaceholder('Enter the default directory')
+				.setValue(this.plugin.settings.dir)
+				.onChange(async (value) => {
+					this.plugin.settings.dir = value;
+					await this.plugin.saveSettings();
+				}));
+
 	}
 }
